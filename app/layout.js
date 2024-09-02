75ab68f893da4e5cb79367efb2b85700d28f18b1
Muhammad Amir <umairamir1127@gmail.com>
2024-09-02 00:38:01 -0400

    title

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,11 @@ import Theming from "@/Theme/Theming";
 import Headpage from "./Header/page";
 import "./globals.css";
 
+export const metadata = {
+  title: "Costivity Waitlist",
+  description: "A waitlist for Costivity - a platform for managing your expenses.",
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
